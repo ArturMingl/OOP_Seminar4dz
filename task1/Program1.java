c3d4012b6a7e372d7347a7812c75669da5db8f77
ArturMingl <adiscov90@gmail.com>
2023-06-18 01:34:55 +0300

    Task1 done

---
+++ task1/Program1.java
@@ -0,0 +1,28 @@
+/*Задача 1 : Напишите обобщенный метод printElement, который принимает один параметр типа T и выводит его на консоль.
+В этом примере мы объявляем обобщенный метод printElement, который принимает один параметр типа T.
+Мы просто выводим этот элемент на консоль с помощью метода System.out.println().
+В методе main мы вызываем printElement с различными типами данных: целое число (Integer), строку (String)
+и число с плавающей запятой (Double). Компилятор автоматически выводит тип параметра T на основе переданных аргументов.
+*/
+
+public class Program1 {
+    public static void main(String[] args) {
+
+        El<String> str = new El<String>("Some string");
+        str.printElement();
+
+    }
+
+    static class El<T> {
+        private T el;
+
+        El(T el) {
+            this.el = el;
+        }
+
+        public void printElement() {
+            System.out.println(el);
+        }
+
+    }
+}
